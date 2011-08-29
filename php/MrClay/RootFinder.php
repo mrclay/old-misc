719aa44cf00d1b1d0e55871044ae360b2ae229ff
steve <steve@mrclay.org>
2011-08-29 04:30:05 +0000

    Fun stuff from FAWN

---
+++ php/MrClay/RootFinder.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Use Newton's Method to find the root of a function
+ * 
+ * E.g. estimate square root of 7.
+ * Find x | y(x) = x^2 - 7 = 0.
+ * 
+ * <code>
+ * class FunctionY {
+ *     // y
+ *     public function call($x) {
+ *         return pow($x, 2) - 7;
+ *     }
+ *     // dy/dx
+ *     public function callDerivative($x) {
+ *         return 2 * $x;
+ *     }
+ * }
+ * $rf = new MrClay_RootFinder();
+ * echo $rf->findRoot(new FunctionY) . "\n";
+ * echo $rf->error . "\n";
+ * print_r($rf->x); // all guesses
+ * </code>
+ *
+ * @link http://en.wikipedia.org/wiki/Newton's_method
+ *
+ * @author Steve Clay <steve@mrclay.org>
+ */
+class MrClay_RootFinder {
+    public $error = null;
+    public $x = array();
+
+    /**
+     * Find an x such that $f->call($x) is within $epsilon of 0
+     *
+     * @param mixed $f function object with methods call($x), callDerivative($x)
+     * @param float $x0 (optional) initial guess of root
+     * @param float $epsilon (optional) acceptable distance from 0
+     * @param int $maxIterations (optional)
+     * @return float
+     */
+    public function findRoot($f, $x0 = 0, $epsilon = 0.00001, $maxIterations = 50)
+    {
+        $this->error = null;
+        if (0 == $f->callDerivative($x0)) {
+            $x0 = 1;
+        }
+        $this->x = array($x0);
+        $i = 0;
+        $errorPrev = null;
+        
+        do {
+            $denominator = $f->callDerivative($this->x[$i]);
+            if (0 == $denominator) {
+                throw new Exception(
+                    'Derivative at ' . $this->x[$i] . ' returned zero. Cannot continue');
+            }
+            $this->x[$i + 1] = $this->x[$i] - $f->call($this->x[$i]) / $denominator;
+            $this->error = abs($this->x[$i + 1] - $this->x[$i]);
+            
+            if ($errorPrev !== null && $errorPrev <= $this->error) {
+                // error is not decreasing, send last x
+                $this->error = $errorPrev;
+                return $this->x[$i];
+            } elseif ($this->error < $epsilon) {
+                // close enough
+                return $this->x[$i + 1];
+            } else {
+                // prepare next iteration
+                $i++;
+                $errorPrev = $this->error;
+            }
+        } while ($i < $maxIterations);
+        
+        return $this->x[$i];
+    }
+}
---
+++ php/MrClay/RootFinder/WetBulb.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Wet Bulb target function (for Newton's method)
+ *
+ * From http://en.wikipedia.org/wiki/Dew_point#Closer_approximation
+ * e = ew - p(T - Tw) .00066 (1 + .00115 Tw)
+ * So...
+ * 0 = ew - e - p(T - Tw) .00066 (1 + .00115 Tw)
+ *
+ * call() evaluates ew - e - bp(T - Tw) .00066 (1 + .00115 Tw) at Tw
+ * callDerivative() evaluates the derivative w/r/t Tw at Tw
+ *
+ * @link http://en.wikipedia.org/wiki/Wet-bulb_temperature
+ */
+class MrClay_RootFinder_WetBulb {
+    public $t2m = null;
+    public $vp = null;
+    public $bp = null;
+
+    /**
+     * @param float $t2m			air temp (C)
+     * @param float $vp				water vapor pressure (mb)
+     * @param float $bp				optional station pressure (mb)
+     */
+    public function __construct($t2m, $vp, $bp = 1013)
+    {
+        $this->t2m = $t2m;
+        $this->vp = $vp;
+        $this->bp = $bp;
+    }
+
+    // http://en.wikipedia.org/wiki/Dew_point#Closer_approximation
+    public function call($Tw)
+    {
+        $T = $this->t2m;
+        $e = $this->vp;
+        $p = $this->bp;
+        $ew = $this->e($Tw);
+        return 
+            $ew - $e - $p * ($T - $Tw) * 0.00066 * (1 + 0.00115 * $Tw);
+    }
+
+    public function callDerivative($Tw)
+    {
+        $T = $this->t2m;
+        $e = $this->vp;
+        $p = $this->bp;
+        return 
+            $this->de_dt($Tw)
+            + $p * 0.00066 * (1 - 0.00115 * ($T - 2 * $Tw));
+    }
+
+    /**
+     * Calculate saturation vapor pressure using Bolton equation
+     *
+     * @param float $t			air temperature (C)
+     * @return float			saturation vapor pressure (mb)
+     * @link http://cires.colorado.edu/~voemel/vp.html
+     */
+    public function e($t)
+    {
+        return 6.112 * exp(17.67 * $t / ($t + 243.5));
+    }
+
+    /**
+     * de/dt evaluated at point $x
+     *
+     * @param float $t			air temperature (C)
+     * @return float			saturation vapor pressure (mb)
+     * @link http://cires.colorado.edu/~voemel/vp.html
+     */
+    public function de_dt($t)
+    {
+        return 6.112                                    // constant
+               * (17.67 * 243.5) / pow($t + 243.5, 2)   // d/dx(exponent)
+               * exp(17.67 * $t / ($t + 243.5));        // exponent
+    }
+}
