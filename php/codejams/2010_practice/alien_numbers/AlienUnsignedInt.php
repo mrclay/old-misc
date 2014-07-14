923e337e280a4ad140a1e20337349dbb4e36d9a3
mrclay.org <mrclay.org@gmail.com>
2014-07-14 03:37:51 +0000

    Clean up old code jam

--- php/codejams/2010_practice/alien_numbers/AlienUnsignedInt.php
+++ php/codejams/2010_practice/alien_numbers/AlienUnsignedInt.php
@@ -2,104 +2,147 @@
 
 /**
  * Unsigned integer with unlimited size and arbitrary (non-space) chars as digits
+ *
+ * Written 2010, Cleaned up 2013
  */
 class AlienUnsignedInt {
-    public $n; // the number (string)
+
+    const UP = 1;
+    const DOWN = -1;
+
+    /**
+     * @var string
+     */
+    public $value;
     
-    // these should really be in an AlienNumberSystem object
-    // so they could be shared between numbers
-    protected $d; // string of digits
-    protected $l = array(); // digits => values. E.g. $hex->l['a'] = 10
-    protected $mod; // num digits in system
-    protected $placeValues = array(); // int values of place digits (up to PHP_INT_MAX)
-    
-    public function __construct($number = null, $digits = '0123456789') {
-        if (! is_string($number)) {
-            $number = $digits[0];
-        }
-        $this->n = (string)$number;
-        $this->d = $digits;
-        $this->l = array_flip(str_split($digits));
-        $this->mod = strlen($digits);
-        // generate placeValues lookup table
+    /**
+     * @var string
+     */
+    protected $digits;
+
+    /**
+     * @var int
+     */
+    protected $numDigits;
+
+    /**
+     * @var int[] value for each digit. E.g. in hex, "9" => 9, "a" => 10...
+     */
+    protected $digitValues = array();
+
+    /**
+     * @var int[] value added by incrementing each digit (up to PHP_INT_MAX).
+     *            E.g. for hex this would be 2 => 16^1, 3 => 16^2, ...
+     */
+    protected $positionValues = array(); // int values of place digits
+
+    /**
+     * @param string $number Number using given digits.
+     * @param string $digits
+     */
+    public function __construct($number, $digits = '0123456789') {
+        $this->value = (string)$number;
+        $this->digits = $digits;
+        $this->digitValues = array_flip(str_split($digits));
+        $this->numDigits = strlen($digits);
+
+        // generate lookup table
         $i = 1;
         while (true) {
-            $v = pow($this->mod, $i);
+            $v = pow($this->numDigits, $i);
             if (! is_int($v) || $v < 1) {
                 // we're beyond ints
                 break;
             }
-            $this->placeValues[$i + 1] = $v;
+            $this->positionValues[$i + 1] = $v;
             $i++;
         }
     }
     
     public function inc() {
-        $this->_incDec();
+        $this->incDecAtIndex(self::UP);
     }
 
     public function dec() {
-        if ($this->n === $this->d[1]) {
-            $this->n = $this->d[0];
+        if ($this->value === $this->digits[1]) {
+            $this->value = $this->digits[0];
             return;
         }
-        $this->_incDec(-1);
+        $this->incDecAtIndex(self::DOWN);
     }
     
-    // increment/decrement the digit at a place (significantly faster than inc/dec)
-    public function incDecPlace($direction /* 1 or -1*/, $place) {
-        $len = strlen($this->n);
-        $placeIdx = $len - $place;
-        if ($direction === 1 && $placeIdx < 0) {
-            // pad with zeros
-            $this->n = str_pad($this->n, $place, $this->d[0], STR_PAD_LEFT);
-            // recompute placeIdx
-            $len = strlen($this->n);
-            $placeIdx = $len - $place;
+    /**
+     * Increment/decrement the digit at a particular position (significantly faster than inc/dec)
+     *
+     * @param int $direction UP or DOWN
+     * @param int $position E.g. 1 for 1s, 2 for 10s column, 3 for 100s column
+     */
+    public function incDecAtPosition($direction, $position) {
+        $len = strlen($this->value);
+        $stringIndex = $len - $position;
+        if ($direction === self::UP && $stringIndex < 0) {
+            // pad with the 0-value digit
+            $this->value = str_pad($this->value, $position, $this->digits[0], STR_PAD_LEFT);
+            // recompute the index
+            $len = strlen($this->value);
+            $stringIndex = $len - $position;
         }
-        $this->_incDec($direction, $placeIdx);
-        if ($direction === -1) {
-            $this->n = ltrim($this->n, $this->d[0]);
-            if ($this->n === '') {
-                $this->n = $this->d[0];
+        $this->incDecAtIndex($direction, $stringIndex);
+        if ($direction === self::DOWN) {
+            // trim leading 0-value digits
+            $this->value = ltrim($this->value, $this->digits[0]);
+            if ($this->value === '') {
+                $this->value = $this->digits[0];
             }
         }
     }
     
-    // get largest int value that can be subtracted by decrementind a place value.
-    // [int, place] E.g. Decimal 86 this would be [10, 2]
-    public function getLargestInt() {
-        $place = strlen($this->n);
-        while ($place) {
-            if (isset($this->placeValues[$place])) {
-                return array($this->placeValues[$place], $place);
+    /**
+     * Get the largest value (and its position) that can be subtracted by decrementing
+     * a position value.
+     *
+     * E.g. Decimal 86 this would be [10, 2], since 10 can be removed by decrementing the
+     * second position.
+     *
+     * @return array [int value, int position] or [false, false]
+     */
+    public function getLargestCachedPositionValue() {
+        $position = strlen($this->value);
+        while ($position) {
+            if (isset($this->positionValues[$position])) {
+                return array($this->positionValues[$position], $position);
             }
-            $place--;
+            $position--;
         }
         return array(false, false);
     }
     
     // check this before dec()!
     public function isZero() {
-        return ($this->n === $this->d[0]);
+        return ($this->value === $this->digits[0]);
     }
     
-    // return equivalent AlienUnsignedInt w/ different digits
-    // (optimized based on number system place values within the PHP integers)
+    /**
+     * Get a value-equivalent AlienUnsignedInt
+     *
+     * @param string $newDigits Digits of the new object
+     * @return AlienUnsignedInt
+     */
     public function convert($newDigits) {
-        $dest = new self(null, $newDigits);
+        $dest = new self($newDigits[0], $newDigits);
         $src = clone $this;
+
         // try optimized transfers
         while (true) {
-            list($int, $place) = $src->getLargestInt();
-            if ($int === false) {
+            list($value, $position) = $src->getLargestCachedPositionValue();
+            if ($value === false) {
                 break;
             }
-            // remove from $src, add to $dest
-            $src->incDecPlace(-1, $place);
-            $dest->add($int);
+            // transfer value from $src, add to $dest
+            $src->incDecAtPosition(self::DOWN, $position);
+            $dest->add($value);
         }
-        // naive value by value transfer
+        // give up and transfer 1 at a time
         while (! $src->isZero()) {
             $src->dec();
             $dest->inc();
@@ -107,60 +150,103 @@ class AlienUnsignedInt {
         return $dest;
     }
     
-    // add decimal value to number
-    // (optimized based on number system place values within the PHP integers)
-    public function add($int) {
-        if ($int < $this->placeValues[2]) {
+    /**
+     * Add a value to the number
+     *
+     * @param int $value
+     */
+    public function add($value) {
+        // avoids needless recursion
+        do {
+            $value = $this->addSome($value);
+        } while ($value);
+    }
+
+    /**
+     * Add a portion of value and return the value left to be added.
+     *
+     * @param int $value
+     * @return int The remainder left to be added
+     */
+    protected function addSome($value) {
+        if ($value < $this->positionValues[2]) {
             // too small to add a place
-            for ($i = 0; $i < $int; $i++) {
+            for ($i = 0; $i < $value; $i++) {
                 $this->inc();
             }
-            return;
+            return 0;
         }
         // we shift a place as necessary then add the remainder
-        // find largest $p : $placeValues[$p] <= int
-        for ($p = count($this->placeValues); $this->placeValues[$p] > $int; $p--) { /* noop */ }
-        $factor = $this->placeValues[$p]; // 256, $p = 3, $int = 1005
-        $numPlaceIncrements = floor($int / $factor); // 3
-        $remainder = $int % $factor; // 237
+
+        // find largest $p such that $placeValues[$p] <= int
+        // e.g. if value = 1005, and the system were hex, the largest value would
+        // be 256 at position 3
+        $position = count($this->positionValues);
+        while ($this->positionValues[$position] > $value) {
+            $position--;
+        }
+
+        $factor = $this->positionValues[$position];
+
+        // if value were 1005 and factor 256, this would mean we should
+        // increment the 3rd position 3 times (768)...
+        $numPlaceIncrements = floor($value / $factor);
+        // ...leaving 237 to increment.
+        $remainder = $value % $factor; // 237
+
         for ($i = 0; $i < $numPlaceIncrements; $i++) {
-            $this->incDecPlace(1, $p);
+            $this->incDecAtPosition(self::UP, $position);
         }
-        if ($remainder) {
-            $this->add($remainder);
-        }
+        return $remainder;
     }
     
     public function __toString() {
-        return $this->n; 
+        return $this->value;
     }
     
-    // [newdigit, didItWrap?]
-    protected function _moduloAdd($oldDigit, $direction /* 1 or -1*/) {
-        $val = $this->l[$oldDigit] + $direction;
-        $wrapped = ($val < 0 || $val == $this->mod);
-        $val = ($val + $this->mod) % $this->mod;
-        return array($this->d[$val], $wrapped);
+    /**
+     * Find the next digit up or down. This will return the digit and whether
+     * or not the position wrapped around.
+     *
+     * @param string $digit The digit to be nudged
+     * @param int $direction UP or DOWN
+     * @return array [string $new_digit, bool $wrap_occurred]
+     */
+    protected function incDecDigit($digit, $direction) {
+        $val = $this->digitValues[$digit] + $direction;
+        $wrap_occurred = ($val < 0 || $val == $this->numDigits);
+        $val = ($val + $this->numDigits) % $this->numDigits;
+        return array($this->digits[$val], $wrap_occurred);
     }
-    
-    protected function _incDec($direction = 1 /* or -1*/, $placeIdx = null) {
-        if (null === $placeIdx) {
-            $placeIdx = strlen($this->n) - 1;
+
+    /**
+     * Increment/decrement the digit at a particular string index in the value
+     *
+     * @param int $direction UP or DOWN
+     * @param int|null $stringIndex If null is given, this will be the index of the
+     *                              last digit. E.g. 2 if the value is "123"
+     */
+    protected function incDecAtIndex($direction, $stringIndex = null) {
+        if (null === $stringIndex) {
+            $stringIndex = strlen($this->value) - 1;
         }
-        if ($placeIdx === -1 && $direction === 1) {
-            // add a place
-            $this->n = $this->d[1] . $this->n;
+        if ($stringIndex === -1 && $direction === self::UP) {
+            // place a 1 digit in the new position on the left
+            $this->value = $this->digits[1] . $this->value;
             return;
         }
-        list($newDigit, $wrapped) = $this->_moduloAdd($this->n[$placeIdx], $direction);
-        $this->n[$placeIdx] = $newDigit;
-        if ($direction === -1 && $placeIdx === 0 && $newDigit === $this->d[0]) {
+
+        $digit = $this->value[$stringIndex];
+        list($digit, $wrap_occurred) = $this->incDecDigit($digit, $direction);
+
+        $this->value[$stringIndex] = $digit;
+        if ($direction === self::DOWN && $stringIndex === 0 && $digit === $this->digits[0]) {
             // remove leading 0
-            $this->n = substr($this->n, 1);
+            $this->value = substr($this->value, 1);
             return;
         }
-        if ($wrapped) {
-            $this->_incDec($direction, $placeIdx - 1);
+        if ($wrap_occurred) {
+            $this->incDecAtIndex($direction, $stringIndex - 1);
         }
     }
 }
